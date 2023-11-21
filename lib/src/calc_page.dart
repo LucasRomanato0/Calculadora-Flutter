a9037d3f081e5fff45d9366638d1e969099197a2
LucasRomanato0 <lucasromanato34@gmail.com>
2023-11-21 08:55:54 -0300

    first commit

---
+++ lib/src/calc_page.dart
@@ -0,0 +1,30 @@
+import 'package:calculadora_flutter/src/widgets/button_hub.dart';
+import 'package:calculadora_flutter/src/widgets/display.dart';
+import 'package:flutter/material.dart';
+
+class CalcPage extends StatefulWidget {
+  const CalcPage({super.key});
+
+  @override
+  State<CalcPage> createState() => _CalcPageState();
+}
+
+class _CalcPageState extends State<CalcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Display(value: '100'),
+          ),
+          Expanded(
+            flex: 3,
+            child: ButtonHub(),
+          ),
+        ],
+      ),
+    );
+  }
+}
